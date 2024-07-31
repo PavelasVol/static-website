34895dcadc49c14c32166a6915d1b08a1d3b63a2
PavelasVol <pavelas@mail.ru>
2024-07-31 21:36:56 +0300

    картинка по размерам окна браузера

--- js/site.js
+++ js/site.js
@@ -1,4 +1,4 @@
 ﻿let d = new Date();
 alert('Нажми на меня!');
-document.body.innerHTML = "<p> <img src='../images/eshera0.png' width='1000' height = '1000' alt = 'Иллюстрация' > </p>";
-document.body.innerHTML = "<h1 style='color:blue'>Сегодня " + d + "</h1 >";
+//document.body.innerHTML = "<p> <img src='../images/eshera.png' width='1000' height = '1000' alt = 'Иллюстрация' > </p>";
+//document.body.innerHTML = "<h1 style='color:blue'>Сегодня " + d + "</h1 >";
