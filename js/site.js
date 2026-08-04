084ae129afa7dca55575cfa29804c0a0ad3182f5
PavelasVol <144889014+PavelasVol@users.noreply.github.com>
2026-08-04 12:19:16 +0300

    Add files via upload

--- js/site.js
+++ js/site.js
@@ -579,7 +579,7 @@ document.addEventListener('click', function (event) {
                 break;
             }
         }
-        alert("x="+x+" y="+y)
+        //alert("x="+x+" y="+y)
         if (found && N > 0) {
             document.getElementById("td00").textContent = names_arr[N] || "Объект " + N;
             document.getElementById("td2").textContent = names_arr2[N] || "Нет описания";
