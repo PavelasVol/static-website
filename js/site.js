98ba06800a473fedaa477ebfb8c22c711b40e8e1
PavelasVol <144889014+PavelasVol@users.noreply.github.com>
2026-08-15 15:22:10 +0300

    Add files via upload

--- js/site.js
+++ js/site.js
@@ -315,7 +315,7 @@ function applyMapTransform() {
             //if (mobileScale < 0.5) mobileScale = 0.5;
             if (mobileScale > 3.0) mobileScale = 3.0;
             // Применяем трансформацию
-            let finalScale = mobileScale * mapScale;
+            let finalScale = mapScale;
             img.style.transform = `translate(${clampedX}px, ${clampedY}px) rotate(90deg) scale(${finalScale})`;
             //alert("Мобильная трансформация: rotate(90deg) scale(" + finalScale.toFixed(2) + ")");
             alert("Мобильная трансформация: translate("+ clampedX.toFixed(0)+", "+clampedY.toFixed(0)+") rotate(90deg) scale("+finalScale.toFixed(3)+")");
@@ -2400,7 +2400,7 @@ names_arr = new Array("",
     names_arr3[63] = new Array("", "Площадь перед кинотеатром с отметкой 1976 года в 2026 году")
 
 // ====== ОБРАБОТЧИК КЛИКОВ ======
-alert("15:10");
+alert("15:20");
 // ====== ОБРАБОТЧИК КЛИКОВ (с учетом масштаба карты) ======
 document.addEventListener('click', function (event) { // Работает хорошо только для ПК, а для мобильных нет попаданий и масштабирования
    //alert("in addEventListener: CLICK");
