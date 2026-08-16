f6a323d68fc868238e8b9b6d2c5c9727934704b0
PavelasVol <144889014+PavelasVol@users.noreply.github.com>
2026-08-16 08:06:09 +0300

    Add files via upload

--- js/site.js
+++ js/site.js
@@ -278,7 +278,7 @@ function applyTransformToMedia() {
 }
 //alert("277");
 // ====== ПРИМЕНЕНИЕ ТРАНСФОРМАЦИЙ К КАРТЕ ======
-alert("01:25");
+alert("08:00");
 function applyMapTransform() {
     let img = document.getElementById('img0');
     if (!img) {
