fad79704e50e4428801a8eacccbc6c85886f64b0
PavelasVol <144889014+PavelasVol@users.noreply.github.com>
2026-08-25 22:36:28 +0300

    Add files via upload

--- js/site.js
+++ js/site.js
@@ -586,7 +586,7 @@ function updateZoomButtonsState() {
     }
 }
 
-alert("22:25");
+alert("22:35");
 // ====== ПРИМЕНЕНИЕ ТРАНСФОРМАЦИЙ К КАРТЕ ======
 function applyMapTransform() {
     let img = document.getElementById('img0');
